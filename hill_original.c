9896b0264b52e50376c31160b2058dc824abeb99
Fu-Chi (Scarlett) Ao <fu-chi.ao@sv.cmu.edu>
2022-04-30 22:08:00 -0700

    Fix hard-coded num count & add original src for comparison

--- hill_original.c
+++ hill_original.c
@@ -8,7 +8,6 @@
 #include <time.h>
 #include <limits.h>
 #include <omp.h>
-#include <immintrin.h>
 
 #define HEADER_LINES 6
 #define NUM_CITIES 13510
@@ -20,18 +19,18 @@ int** city; // city[id][0] = x of city id, city[id][1] = y of city id,
 int** city_dis; // distance between city, city_dis[id1][id2] = distance(id1,id2)
 int city_count;
 
-inline int distance(int a, int b)
+int distance(int a, int b)
 {
 	int x_dis = city[a][0] - city[b][0];
 	int y_dis = city[a][1] - city[b][1];
 	return round(sqrt(x_dis * x_dis + y_dis * y_dis));
 }
 
-inline int route_distance(int *city)
+int route_distance(int *city)
 {
 	int i, tot_dis = 0;
 	for (i = 0; i < city_count - 1; i++)
-		tot_dis += city_dis[city[i]][city[i + 1]]; // this reduction is memory bound
+		tot_dis += city_dis[city[i]][city[i + 1]];
 	tot_dis += city_dis[city[0]][city[city_count - 1]];	// dist for tail ~ head
 	return tot_dis;
 }
@@ -66,7 +65,7 @@ int neighbor_dis(int *a, int p1, int p2, int ori_part)
 
 int *get_neighbor(int *a)
 {
-	int i, j, j_right;
+	int i, j, j_right, k;
 	float ori_base;
 	for (i = 0; i < city_count - 1; i++)
 	{
@@ -102,7 +101,7 @@ int *get_start_state()
 
 int hill()
 {
-	int ans;
+	int i, j, ans;
 	int *cur_state = get_start_state();
 	while (1)
 	{
@@ -143,7 +142,7 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 
-	double start = omp_get_wtime(), stop;
+	clock_t start = clock(), stop;
 	srand(time(NULL));
 	char line[200];
 	int jmp_counter = HEADER_LINES, i, j;
@@ -167,53 +166,24 @@ int main(int argc, char **argv)
 		{
 			sscanf(line, "%*d %E %E", &x, &y);
 			city[city_count][0] = (int)x, city[city_count++][1] = (int)y;
-			//#pragma omp for parallel shared(city_dis)
-			for (i = 0; i < city_count - 4; i+=4) {
-				float temp[4];
-				__m128 d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
-
-				d1 = _mm_setr_ps(city[i][0], city[i+1][0], city[i+2][0], city[i+3][0]); // (x1,x2,x3,x4)
-				d2 = _mm_set_ps1(city[city_count - 1][0]); // (x999,x999,x999,x999)
-
-				d3 = _mm_setr_ps(city[i][1], city[i+1][1], city[i+2][1], city[i+3][1]); // (y1,y1,y2,y2)
-				d4 = _mm_set_ps1(city[city_count - 1][1]); // (y999,y999,y999,y999)
-
-				d5 = _mm_sub_ps(d1, d2); // (x1-x999,x2-x999,x3-x999,x4-x999)
-				d6 = _mm_sub_ps(d3, d4); // (y1-y999,y2-y999,y3-y999,y4-y999)
-
-				d7 = _mm_mul_ps(d5, d5);
-				d8 = _mm_mul_ps(d6, d6);
-
-				d9 = _mm_add_ps(d7, d8);
-				d10 = _mm_sqrt_ps(d9);
-
-				_mm_store_ps(&temp[0], d10); // store sum into an array
-
-				city_dis[city_count - 1][i] = city_dis[i][city_count - 1] = round(temp[0]);
-				city_dis[city_count - 1][i+1] = city_dis[i+1][city_count - 1] = round(temp[1]);
-				city_dis[city_count - 1][i+2] = city_dis[i+2][city_count - 1] = round(temp[2]);
-				city_dis[city_count - 1][i+3] = city_dis[i+3][city_count - 1] = round(temp[3]);
-			}
-			for (; i < city_count - 1; i++)
+			// TODO: change to 1 dimension for speedup
+			for (i = 0; i < city_count - 1; i++)
 				city_dis[city_count - 1][i] = city_dis[i][city_count - 1] = distance(i, city_count - 1);
 		}
 	}
 
-	int counter, distance, min = INT_MAX;
+	int counter = num_runs, distance, min = 9999999;
 	float tot = 0;
-
 	printf("Final distances: ");
-	#pragma omp parallel for shared(min,tot) private(distance)
-	for(counter = 0; counter < num_runs; counter++) {
+	while (counter--) {
 		distance = hill();
-		printf(" %d", distance);
-
 		if (distance < min)
 			min = distance;
 		tot += (float)distance;
+		printf(" %d", distance);
 	}
 	printf("\nmin: %d avg: %f\n", min, tot / num_runs);
-	stop = omp_get_wtime();
-	printf("total execution time: %f\n", (double)(stop - start) );
+	stop = clock();
+	printf("total execution time: %f\n", (double)(stop - start) / (double)CLOCKS_PER_SEC);
 	return 0;
 }
