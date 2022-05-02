8b728e044412f22cc3ade1a51aa9df45bbf49986
Fu-Chi (Scarlett) Ao <fu-chi.ao@sv.cmu.edu>
2022-05-01 20:31:35 -0700

    Speed up around x7

--- hill.c
+++ hill.c
@@ -20,19 +20,20 @@ int** city; // city[id][0] = x of city id, city[id][1] = y of city id,
 int** city_dis; // distance between city, city_dis[id1][id2] = distance(id1,id2)
 int city_count;
 
-inline int distance(int a, int b)
+__attribute__((always_inline)) inline int distance(int a, int b)
 {
 	int x_dis = city[a][0] - city[b][0];
 	int y_dis = city[a][1] - city[b][1];
 	return round(sqrt(x_dis * x_dis + y_dis * y_dis));
 }
 
-inline int route_distance(int *city)
+__attribute__((always_inline)) inline int route_distance(int *city)
 {
 	int i, tot_dis = 0;
-	for (i = 0; i < city_count - 1; i++)
+	int lastIdx = city_count - 1;
+	for (i = 0; i < lastIdx; i++)
 		tot_dis += city_dis[city[i]][city[i + 1]]; // this reduction is memory bound
-	tot_dis += city_dis[city[0]][city[city_count - 1]];	// dist for tail ~ head
+	tot_dis += city_dis[city[0]][city[lastIdx]];	// dist for tail ~ head
 	return tot_dis;
 }
 
@@ -50,7 +51,7 @@ int *gen_neighbor(int *a, int i, int j)
 	return neighbor;
 }
 
-int neighbor_dis(int *a, int p1, int p2, int ori_part)
+__attribute__((always_inline)) inline int neighbor_dis(int *a, int p1, int p2, int ori_part)
 {
 	int p1_left, p2_right, tmp, ans;
 	if (p1 > p2)
@@ -59,8 +60,15 @@ int neighbor_dis(int *a, int p1, int p2, int ori_part)
 		p1 = p2;
 		p2 = tmp;
 	}
-	p1_left = (p1 == 0 ? city_count - 1 : p1 - 1);
-	p2_right = (p2 == city_count - 1 ? 0 : p2 + 1);
+	int lastCity = city_count - 1;
+	p1_left = p1 - 1;
+	if (p1 == 0) {
+		p1_left = lastCity;
+	}
+	p2_right = p2 + 1;
+	if (p2 == lastCity) {
+		p2_right = 0;
+	}
 	return city_dis[a[p1_left]][a[p2]] + city_dis[a[p2_right]][a[p1]] - ori_part;
 }
 
@@ -68,15 +76,24 @@ int *get_neighbor(int *a)
 {
 	int i, j, j_right;
 	float ori_base;
-	for (i = 0; i < city_count - 1; i++)
-	{
-		ori_base = i == 0 ? city_dis[a[i]][a[city_count - 1]] : city_dis[a[i]][a[i - 1]];
-		for (j = i + 1; j < city_count; j++)
-			if (j - i < city_count - 2)
-			{
-				j_right = (j == city_count - 1 ? 0 : j + 1);
-				if (neighbor_dis(a, i, j, ori_base + city_dis[a[j]][a[j_right]]) < 0)
-					return gen_neighbor(a, i, j);
+
+	int lastIdx = city_count - 1;
+	int beforeLastIdx = city_count - 2;
+
+	for (i = 0; i < lastIdx; i++) {
+			ori_base = city_dis[a[i]][a[i - 1]];
+			if (i == 0) {
+				ori_base = city_dis[a[i]][a[lastIdx]];
+			}
+			for (j = i + 1; j < city_count; j++) {
+				if (j - i < beforeLastIdx) {
+					j_right = j + 1;
+					if (j == lastIdx) {
+						j_right = 0;
+					}
+					if (neighbor_dis(a, i, j, ori_base + city_dis[a[j]][a[j_right]]) < 0)
+						return gen_neighbor(a, i, j);
+				}
 			}
 	}
 	return NULL;
@@ -100,7 +117,7 @@ int *get_start_state()
 	return start_state;
 }
 
-int hill()
+__attribute__((always_inline)) inline int hill()
 {
 	int ans;
 	int *cur_state = get_start_state();
@@ -167,16 +184,58 @@ int main(int argc, char **argv)
 		{
 			sscanf(line, "%*d %E %E", &x, &y);
 			city[city_count][0] = (int)x, city[city_count++][1] = (int)y;
+			int lastCity = city_count - 1;
 			//#pragma omp for parallel shared(city_dis)
 			for (i = 0; i < city_count - 4; i+=4) {
-				float temp[4];
+				float temp[8];
+				__m128 d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16;
+
+				d1 = _mm_setr_ps(city[i][0], city[i+1][0], city[i+2][0], city[i+3][0]);  // (x1,x2,x3,x4)
+				d2 = _mm_set_ps1(city[lastCity][0]); // (x999,x999,x999,x999)
+
+				d3 = _mm_setr_ps(city[i][1], city[i+1][1], city[i+2][1], city[i+3][1]);  // (y1,y1,y2,y2)
+				d4 = _mm_set_ps1(city[lastCity][1]); // (y999,y999,y999,y999)
+
+				d5 = _mm_sub_ps(d1, d2);  // (x1-x999,x2-x999,x3-x999,x4-x999)
+				d1 = _mm_mul_ps(d5, d5);
+				d6 = _mm_sub_ps(d3, d4);  // (y1-y999,y2-y999,y3-y999,y4-y999)
+				d3 = _mm_mul_ps(d6, d6);
+				d7 = _mm_add_ps(d1, d3);
+				d8 = _mm_sqrt_ps(d7);
+
+				d9 = _mm_setr_ps(city[i+4][0], city[i+5][0], city[i+6][0], city[i+7][0]);  // (x5,x6,x7,x8)
+				d10 = _mm_setr_ps(city[i+4][1], city[i+5][1], city[i+6][1], city[i+7][1]); // (y5,y6,y7,y8)
+
+				d11 = _mm_sub_ps(d9, d2);  // (x5-x999,x6-x999,x7-x999,x8-x999)
+				d9 = _mm_mul_ps(d11, d11);
+				d12 = _mm_sub_ps(d10, d4); // (y5-y999,y6-y999,y7-y999,y8-y999)
+				d10 = _mm_mul_ps(d12, d12);
+				d13 = _mm_add_ps(d9, d10);
+				d14 = _mm_sqrt_ps(d13);
+
+				_mm_store_ps(&temp[0], d8);  // store sum into an array
+				_mm_store_ps(&temp[4], d14); // store sum into an array
+
+				city_dis[lastCity][i] = city_dis[i][lastCity] = round(temp[0]);
+				city_dis[lastCity][i+1] = city_dis[i+1][lastCity] = round(temp[1]);
+				city_dis[lastCity][i+2] = city_dis[i+2][lastCity] = round(temp[2]);
+				city_dis[lastCity][i+3] = city_dis[i+3][lastCity] = round(temp[3]);
+				city_dis[lastCity][i+4] = city_dis[i+4][lastCity] = round(temp[4]);
+				city_dis[lastCity][i+5] = city_dis[i+5][lastCity] = round(temp[5]);
+				city_dis[lastCity][i+6] = city_dis[i+6][lastCity] = round(temp[6]);
+				city_dis[lastCity][i+7] = city_dis[i+7][lastCity] = round(temp[7]);
+			}
+
+			// Handle the rest
+			for (; i < city_count - 4; i+=4) {
+			  float temp[4];
 				__m128 d1, d2, d3, d4, d5, d6, d7, d8, d9, d10;
 
 				d1 = _mm_setr_ps(city[i][0], city[i+1][0], city[i+2][0], city[i+3][0]); // (x1,x2,x3,x4)
-				d2 = _mm_set_ps1(city[city_count - 1][0]); // (x999,x999,x999,x999)
+				d2 = _mm_set_ps1(city[lastCity][0]); // (x999,x999,x999,x999)
 
 				d3 = _mm_setr_ps(city[i][1], city[i+1][1], city[i+2][1], city[i+3][1]); // (y1,y1,y2,y2)
-				d4 = _mm_set_ps1(city[city_count - 1][1]); // (y999,y999,y999,y999)
+				d4 = _mm_set_ps1(city[lastCity][1]); // (y999,y999,y999,y999)
 
 				d5 = _mm_sub_ps(d1, d2); // (x1-x999,x2-x999,x3-x999,x4-x999)
 				d6 = _mm_sub_ps(d3, d4); // (y1-y999,y2-y999,y3-y999,y4-y999)
@@ -189,13 +248,14 @@ int main(int argc, char **argv)
 
 				_mm_store_ps(&temp[0], d10); // store sum into an array
 
-				city_dis[city_count - 1][i] = city_dis[i][city_count - 1] = round(temp[0]);
-				city_dis[city_count - 1][i+1] = city_dis[i+1][city_count - 1] = round(temp[1]);
-				city_dis[city_count - 1][i+2] = city_dis[i+2][city_count - 1] = round(temp[2]);
-				city_dis[city_count - 1][i+3] = city_dis[i+3][city_count - 1] = round(temp[3]);
+				city_dis[lastCity][i] = city_dis[i][lastCity] = round(temp[0]);
+				city_dis[lastCity][i+1] = city_dis[i+1][lastCity] = round(temp[1]);
+				city_dis[lastCity][i+2] = city_dis[i+2][lastCity] = round(temp[2]);
+				city_dis[lastCity][i+3] = city_dis[i+3][lastCity] = round(temp[3]);
 			}
-			for (; i < city_count - 1; i++)
-				city_dis[city_count - 1][i] = city_dis[i][city_count - 1] = distance(i, city_count - 1);
+			for (; i < lastCity; i++) {
+				city_dis[lastCity][i] = city_dis[i][lastCity] = distance(i, lastCity);
+			}
 		}
 	}
 
@@ -204,7 +264,7 @@ int main(int argc, char **argv)
 
 	printf("Final distances: ");
 	#pragma omp parallel for shared(min,tot) private(distance)
-	for(counter = 0; counter < num_runs; counter++) {
+	for (counter = 0; counter < num_runs; counter++) {
 		distance = hill();
 		printf(" %d", distance);
 
